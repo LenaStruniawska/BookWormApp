a4b09bdd212798c1e71238f62cdb6d2ee7bda84b
Magdalena Struniawska <mag.sok@onet.pl>
2022-04-20 08:01:16 +0200

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Slawek on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
