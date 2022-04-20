ccbf2421238cae3a3c9399a26ba3c58d385b6c32
Magdalena Struniawska <mag.sok@onet.pl>
2022-04-20 14:05:20 +0200

    BookWorm App

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+var body: some View {
+    Text("Hello, world!")
+        .padding()
+  }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ BookWorm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  BookWorm
+//
+//  Created by Slawek on 20/04/2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { descriptoin, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
